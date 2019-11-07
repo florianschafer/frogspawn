85d889ec7b1fd62b0afb17499f4c6f74d2880fef
Florian Schaefer <florian@adeptropolis.net>
2019-11-07 23:21:32 +0100

    Some notes, tex documentation

--- src/main/java/net/adeptropolis/nephila/graphs/operators/LinearGraphOperator.java
+++ src/main/java/net/adeptropolis/nephila/graphs/operators/LinearGraphOperator.java
@@ -13,6 +13,7 @@ public interface LinearGraphOperator {
    * @return Apply the given operator to the argument vector and return the result.
    */
 
+  // TODO: Supply the preallocated result vector as second argument, so it may be re-used at a higher level
   double[] apply(double[] argument);
 
   int size();
--- src/test/java/net/adeptropolis/nephila/graphs/algorithms/RowWeightsTest.java
+++ src/test/java/net/adeptropolis/nephila/graphs/algorithms/RowWeightsTest.java
@@ -10,6 +10,7 @@ import java.util.function.Consumer;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+@Deprecated
 public class RowWeightsTest {
 
   @Test
