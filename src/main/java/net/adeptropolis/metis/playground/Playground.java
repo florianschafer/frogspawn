f33c529749d723e3c4411ac35571c1daf3fd0bd6
Florian Schaefer <florian@adeptropolis.net>
2020-04-07 20:50:12 +0200

    Move playground stuff from tests into a proper source package

--- src/main/java/net/adeptropolis/metis/playground/Playground.java
+++ src/main/java/net/adeptropolis/metis/playground/Playground.java
@@ -1,10 +1,11 @@
 /*
- * Copyright Florian Schaefer 2019.
+ * Copyright (c) Florian Schaefer 2020.
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package net.adeptropolis.metis;
+package net.adeptropolis.metis.playground;
 
+import net.adeptropolis.metis.ClusteringSettings;
 import net.adeptropolis.metis.clustering.Cluster;
 import net.adeptropolis.metis.clustering.RecursiveClustering;
 import net.adeptropolis.metis.digest.ClusterDigester;
@@ -12,8 +13,6 @@ import net.adeptropolis.metis.graphs.implementations.CompressedSparseGraph;
 import net.adeptropolis.metis.graphs.implementations.CompressedSparseGraphBuilder;
 import net.adeptropolis.metis.sinks.Sink;
 import net.adeptropolis.metis.sinks.TextSink;
-import org.junit.Ignore;
-import org.junit.Test;
 
 import java.io.FileNotFoundException;
 import java.nio.file.Paths;
@@ -26,15 +25,16 @@ import static net.adeptropolis.metis.digest.ClusterDigester.DESCENDING_COMBINED;
  * !!!
  */
 
-@Ignore("Manual testing")
 public class Playground {
 
-  @Test
-  @SuppressWarnings("squid:S2699")
-  public void standardClustering() throws FileNotFoundException {
-    LabeledTSVGraphSource g = new LabeledTSVGraphSource(Paths.get("/home/florian/Datasets/Workbench/wiki_corenlp.filtered.graph.very_large.tsv"));
+  public static void main(String[] args) throws FileNotFoundException {
+    new Playground().standardClustering();
+  }
+
+  private void standardClustering() throws FileNotFoundException {
+//    LabeledTSVGraphSource g = new LabeledTSVGraphSource(Paths.get("/home/florian/Datasets/Workbench/wiki_corenlp.filtered.graph.very_large.tsv"));
 //    LabeledTSVGraphSource g = new LabeledTSVGraphSource(Paths.get("/home/florian/Datasets/Workbench/wiki_corenlp.filtered.graph.10M.tsv"));
-//    LabeledTSVGraphSource g = new LabeledTSVGraphSource(Paths.get("/home/florian/Datasets/Workbench/wiki_corenlp.filtered.graph.small.tsv"));
+    LabeledTSVGraphSource g = new LabeledTSVGraphSource(Paths.get("/home/florian/Datasets/Workbench/wiki_corenlp.filtered.graph.small.tsv"));
     ClusteringSettings settings = ClusteringSettings.builder()
             .withMinVertexConsistency(0.05)
             .withMinparentOverlap(0.65)
@@ -45,10 +45,10 @@ public class Playground {
     Cluster root = new RecursiveClustering(graph, settings).run();
 //    ClusterDigester digester = new ClusterDigester(settings.getConsistencyMetric(), 1000, false, DESCENDING_WEIGHTS);
     ClusterDigester digester = new ClusterDigester(settings.getConsistencyMetric(), 1000, false, DESCENDING_COMBINED.apply(1.75));
-    Sink textSink = new TextSink(Paths.get("/home/florian/tmp/clusters14.txt"), digester, g.inverseLabels());
+    Sink textSink = new TextSink(Paths.get("/home/florian/tmp/clusters15.txt"), digester, g.inverseLabels());
 //    LeafTextSink textSink = new LeafTextSink(Paths.get("/home/florian/tmp/clusters16.txt"), digester, g.inverseLabels());
     textSink.consume(root);
-  }
 
+  }
 
 }
---
+++ src/main/java/net/adeptropolis/metis/playground/README.txt
@@ -0,0 +1,2 @@
+Ignore the contents of this package. All classes here are just for playing around
+and will be removed sooner than later.
