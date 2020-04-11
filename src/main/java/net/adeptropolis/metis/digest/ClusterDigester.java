028a4ee2c44903cccd6c507fb4a7989e3f77d58e
Florian Schaefer <florian@adeptropolis.net>
2020-04-11 22:35:15 +0200

    Digester fix: Remainder digests weren't using aggregate weights/scores

--- src/main/java/net/adeptropolis/metis/digest/ClusterDigester.java
+++ src/main/java/net/adeptropolis/metis/digest/ClusterDigester.java
@@ -9,6 +9,7 @@ import net.adeptropolis.metis.ClusteringSettings;
 import net.adeptropolis.metis.clustering.Cluster;
 import net.adeptropolis.metis.clustering.consistency.ConsistencyMetric;
 import net.adeptropolis.metis.graphs.Graph;
+import net.adeptropolis.metis.graphs.VertexIterator;
 import net.adeptropolis.metis.helpers.Arr;
 
 import java.util.function.Function;
@@ -52,10 +53,51 @@ public class ClusterDigester {
    */
 
   public Digest digest(Cluster cluster) {
-    Graph graph = aggregate ? cluster.aggregateGraph() : cluster.remainderGraph();
-    int[] vertices = graph.collectVertices();
-    double[] weights = graph.weights();
-    double[] consistencyScores = metric.compute(cluster.rootGraph(), graph);
+    return aggregate ? aggregateDigest(cluster) : remainderDigest(cluster);
+  }
+
+  /**
+   * Create a new digest, considering a cluster's aggregate graph.
+   *
+   * @param cluster Cluster from which to create the new digest
+   * @return New digest instance.
+   */
+
+  private Digest aggregateDigest(Cluster cluster) {
+    Graph aggregateGraph = cluster.aggregateGraph();
+    int[] vertices = aggregateGraph.collectVertices();
+    double[] weights = aggregateGraph.weights();
+    double[] consistencyScores = metric.compute(cluster.rootGraph(), aggregateGraph);
+    return finalizeDigest(vertices, weights, consistencyScores);
+  }
+
+  /**
+   * Create a new digest, considering only a cluster's remainder.
+   * <p>Note: Vertex weights and consistency scores still refer to the aggregate graph of the cluster.</p>
+   *
+   * @param cluster Cluster from which to create the new digest
+   * @return New digest instance.
+   */
+
+  private Digest remainderDigest(Cluster cluster) {
+    Graph aggregateGraph = cluster.aggregateGraph();
+    Graph remainderGraph = cluster.remainderGraph();
+    int[] vertices = remainderGraph.collectVertices();
+    double[] weights = restrictedWeights(aggregateGraph, remainderGraph);
+    double[] consistencyScores = metric.compute(cluster.rootGraph(), aggregateGraph, remainderGraph);
+    return finalizeDigest(vertices, weights, consistencyScores);
+  }
+
+  /**
+   * Finalize cluster vertices and create a new digest: Sort + limit
+   *
+   * @param vertices          Digest vertices
+   * @param weights           Digest weights
+   * @param consistencyScores Digest consistency scores
+   * @return New digest instance
+   */
+
+  private Digest finalizeDigest(int[] vertices, double[] weights, double[] consistencyScores) {
     MemberSortOps.sort(vertices, weights, consistencyScores, ranking);
     if (maxSize > 0) {
       return subsetDigest(vertices, weights, consistencyScores, vertices.length);
@@ -79,6 +121,23 @@ public class ClusterDigester {
             Arr.shrink(weights, maxSize),
             Arr.shrink(consistencyScores, maxSize),
             totalSize);
+  }
+
+  /**
+   * Restrict weights of an aggregate graph to a remainder-only graph
+   *
+   * @param aggregateGraph Aggregate graph of a cluster
+   * @param remainderGraph Remainder graph of the same cluster
+   * @return Aggregate graph weights, restricted to the remainder graph
+   */
+
+  private double[] restrictedWeights(Graph aggregateGraph, Graph remainderGraph) {
+    double[] weights = new double[remainderGraph.order()];
+    VertexIterator it = remainderGraph.vertexIterator();
+    while (it.hasNext()) {
+      weights[it.localId()] = aggregateGraph.weightForGlobalId(it.globalId());
+    }
+    return weights;
   }
 
 }
--- src/test/java/net/adeptropolis/metis/digest/ClusterDigesterTest.java
+++ src/test/java/net/adeptropolis/metis/digest/ClusterDigesterTest.java
@@ -10,11 +10,14 @@ import net.adeptropolis.metis.ClusteringSettings;
 import net.adeptropolis.metis.clustering.Cluster;
 import net.adeptropolis.metis.clustering.consistency.ConsistencyMetric;
 import net.adeptropolis.metis.clustering.consistency.RelativeWeightConsistencyMetric;
+import net.adeptropolis.metis.graphs.Graph;
 import net.adeptropolis.metis.graphs.implementations.CompressedSparseGraph;
 import net.adeptropolis.metis.graphs.implementations.CompressedSparseGraphBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static net.adeptropolis.metis.digest.ClusterDigester.WEIGHT_RANKING;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -23,52 +26,55 @@ import static org.hamcrest.Matchers.is;
 
 public class ClusterDigesterTest {
 
-  private static final ClusteringSettings settings = ClusteringSettings.builder()
-          .withConsistencyMetric(new RelativeWeightConsistencyMetric())
+  private static final ClusteringSettings aggregate3Settings = ClusteringSettings.builder()
           .withMaxDigestSize(3)
           .withAggregateDigests(true)
           .withDigestRanking(WEIGHT_RANKING)
           .build();
 
+  private static final ClusteringSettings remainder2Settings = ClusteringSettings.builder()
+          .withMaxDigestSize(2)
+          .withAggregateDigests(false)
+          .withDigestRanking(WEIGHT_RANKING)
+          .build();
 
-  @Test
-  public void aggregate() {
-    CompressedSparseGraph graph = new CompressedSparseGraphBuilder()
-            .add(0, 1, 1)
-            .add(0, 2, 2)
-            .add(0, 3, 3)
-            .add(0, 4, 4)
-            .add(0, 5, 5)
-            .add(0, 6, 6)
-            .add(1, 2, 7)
-            .add(1, 3, 8)
-            .add(1, 4, 9)
-            .add(1, 5, 10)
-            .add(1, 6, 11)
+  private Graph graph;
+  private Cluster c2;
+
+  @Before
+  public void setup() {
+    graph = new CompressedSparseGraphBuilder()
+            .add(0, 1, 1).add(0, 2, 2).add(0, 3, 3).add(0, 4, 4).add(0, 5, 5).add(0, 6, 6)
+            .add(1, 2, 7).add(1, 3, 8).add(1, 4, 9).add(1, 5, 10).add(1, 6, 11)
             .add(2, 3, 12)
-            .add(4, 5, 13)
-            .add(4, 6, 14)
-            .add(4, 7, 15)
-            .add(4, 8, 16)
-            .add(4, 9, 17)
-            .add(5, 6, 18)
-            .add(5, 7, 19)
-            .add(5, 8, 20)
-            .add(5, 9, 21)
+            .add(4, 5, 13).add(4, 6, 14).add(4, 7, 15).add(4, 8, 16).add(4, 9, 17)
+            .add(5, 6, 18).add(5, 7, 19).add(5, 8, 20).add(5, 9, 21)
             .add(6, 7, 22)
-            .add(8, 9, 23)
-            .build();
+            .add(8, 9, 23).build();
     Cluster root = new Cluster(graph);
     root.addToRemainder(IntIterators.wrap(new int[]{0, 1}));
     Cluster c1 = new Cluster(root);
     c1.addToRemainder(IntIterators.wrap(new int[]{2, 3}));
-    Cluster c2 = new Cluster(root);
+    c2 = new Cluster(root);
     c2.addToRemainder(IntIterators.wrap(new int[]{4, 5}));
     Cluster c21 = new Cluster(c2);
-    c2.addToRemainder(IntIterators.wrap(new int[]{6, 7}));
+    c21.addToRemainder(IntIterators.wrap(new int[]{6, 7}));
     Cluster c22 = new Cluster(c2);
-    c2.addToRemainder(IntIterators.wrap(new int[]{8, 9}));
-    Digest digest = new ClusterDigester(settings).digest(c2);
+    c22.addToRemainder(IntIterators.wrap(new int[]{8, 9}));
+  }
+
+  @Test
+  public void aggregateEmptyGraph() {
+    Cluster root = new Cluster(new CompressedSparseGraphBuilder().build());
+    Digest digest = new ClusterDigester(aggregate3Settings).digest(root);
+    assertThat(digest.getVertices().length, is(0));
+    assertThat(digest.getWeights().length, is(0));
+    assertThat(digest.getScores().length, is(0));
+  }
+
+  @Test
+  public void aggregate() {
+    Digest digest = new ClusterDigester(aggregate3Settings).digest(c2);
     assertThat(digest.getVertices().length, is(3));
     assertThat(digest.getVertices()[0], is(5));
     assertThat(digest.getVertices()[1], is(4));
@@ -78,80 +84,64 @@ public class ClusterDigesterTest {
     assertThat(digest.getWeights()[1], closeTo(75, 1E-9));
     assertThat(digest.getWeights()[2], closeTo(61, 1E-9));
     assertThat(digest.getScores().length, is(3));
-    assertThat(digest.getScores()[0], closeTo(0.8584905660377359, 1E-9));
-    assertThat(digest.getScores()[1], closeTo(0.8522727272727273, 1E-9));
+    assertThat(digest.getScores()[0], closeTo(0.858490566, 1E-9));
+    assertThat(digest.getScores()[1], closeTo(0.852272727, 1E-9));
     assertThat(digest.getScores()[2], closeTo(1.0, 1E-9));
   }
 
   @Test
-  public void aggregateSmallGraph() {
-    CompressedSparseGraph graph = new CompressedSparseGraphBuilder()
-            .add(0, 1, 1)
-            .add(0, 2, 2)
-            .build();
-    Cluster root = new Cluster(graph);
-    root.addToRemainder(IntIterators.wrap(new int[]{0, 1}));
-    Digest digest = new ClusterDigester(settings).digest(root);
-    assertThat(digest.getVertices().length, is(2));
-    assertThat(digest.getWeights().length, is(2));
-    assertThat(digest.getScores().length, is(2));
-  }
-
-  @Test
-  public void aggregateEmptyGraph() {
-    CompressedSparseGraph graph = new CompressedSparseGraphBuilder().build();
-    Cluster root = new Cluster(graph);
-    root.addToRemainder(IntIterators.wrap(new int[]{}));
-    Digest digest = new ClusterDigester(settings).digest(root);
+  public void remainderEmptyGraph() {
+    Cluster root = new Cluster(new CompressedSparseGraphBuilder().build());
+    Digest digest = new ClusterDigester(remainder2Settings).digest(root);
     assertThat(digest.getVertices().length, is(0));
     assertThat(digest.getWeights().length, is(0));
     assertThat(digest.getScores().length, is(0));
   }
 
   @Test
-  public void remainderOnly() {
-    Digest digest = defaultDigest();
-    assertThat(digest.size(), is(2));
-    assertThat(digest.totalSize(), is(4));
-    assertThat(digest.getVertices()[0], is(0));
-    assertThat(digest.getVertices()[1], is(3));
-    assertThat(digest.getWeights()[0], closeTo(6, 1E-6));
-    assertThat(digest.getWeights()[1], closeTo(3, 1E-6));
+  public void remainder() {
+    Digest digest = new ClusterDigester(remainder2Settings).digest(c2);
+    assertThat(digest.getVertices().length, is(2));
+    assertThat(digest.getVertices()[0], is(5));
+    assertThat(digest.getVertices()[1], is(4));
+    assertThat(digest.getWeights()[0], closeTo(91, 1E-9));
+    assertThat(digest.getWeights()[1], closeTo(75, 1E-9));
+    assertThat(digest.getScores().length, is(2));
+    assertThat(digest.getScores()[0], closeTo(0.858490566, 1E-9));
+    assertThat(digest.getScores()[1], closeTo(0.852272727, 1E-9));
+  }
+
+  @Test
+  public void unlimitedSize() {
+    ClusteringSettings unlimitedSettings = ClusteringSettings.builder()
+            .withMaxDigestSize(0)
+            .withAggregateDigests(true)
+            .withDigestRanking(WEIGHT_RANKING)
+            .build();
+    Digest digest = new ClusterDigester(unlimitedSettings).digest(c2);
+    assertThat(digest.size(), is(6));
+    assertThat(digest.size(), is(digest.totalSize()));
   }
 
   @Test
   public void mapping() {
-    String digestFingerprint = defaultDigest()
-            .map((vertexId, weight, score) -> String.format("%d|%.1f|%.1f", vertexId, weight, score))
+    String digestFingerprint = new ClusterDigester(aggregate3Settings)
+            .digest(c2)
+            .map((vertexId, weight, score) -> String.format("%d|%.1f|%.3f", vertexId, weight, score))
             .collect(Collectors.joining(","));
-    assertThat(digestFingerprint, is("0|6.0|1.0,3|3.0|1.0"));
+    assertThat(digestFingerprint, is("5|91.0|0.858,4|75.0|0.852,9|61.0|1.000"));
   }
 
   @Test
   public void labeledMapping() {
-    String[] labels = new String[]{"[0]", "[1]", "[2]", "[3]"};
-    String digestFingerprint = defaultDigest()
-            .map((label, weight, score) -> String.format("%s|%.1f|%.1f", label, weight, score), labels)
+    String[] labels = IntStream
+            .range(0, graph.order())
+            .mapToObj(i -> String.format("[%d]", i)).toArray(String[]::new);
+    String digestFingerprint = new ClusterDigester(aggregate3Settings)
+            .digest(c2)
+            .map((label, weight, score) -> String.format("%s|%.1f|%.3f", label, weight, score), labels)
             .collect(Collectors.joining(","));
-    assertThat(digestFingerprint, is("[0]|6.0|1.0,[3]|3.0|1.0"));
-  }
-
-  private Digest defaultDigest() {
-    ConsistencyMetric metric = new RelativeWeightConsistencyMetric();
-    CompressedSparseGraph graph = new CompressedSparseGraphBuilder()
-            .add(0, 1, 1)
-            .add(0, 2, 2)
-            .add(0, 3, 3)
-            .build();
-    Cluster root = new Cluster(graph);
-    root.addToRemainder(IntIterators.wrap(new int[]{0, 1, 2, 3}));
-    ClusteringSettings altSettings = ClusteringSettings.builder()
-            .withConsistencyMetric(new RelativeWeightConsistencyMetric())
-            .withMaxDigestSize(2)
-            .withAggregateDigests(false)
-            .withDigestRanking(WEIGHT_RANKING)
-            .build();
-    return new ClusterDigester(altSettings).digest(root);
+    assertThat(digestFingerprint, is("[5]|91.0|0.858,[4]|75.0|0.852,[9]|61.0|1.000"));
   }
 
 }
