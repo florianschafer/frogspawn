bcfe2fc6cda920ec12bb517b85bd08049dfbed4c
Florian Schaefer <florian@adeptropolis.net>
2020-04-11 17:48:37 +0200

    Optional vertex restriction on consistency metrics

--- src/main/java/net/adeptropolis/metis/clustering/consistency/RelativeWeightConsistencyMetric.java
+++ src/main/java/net/adeptropolis/metis/clustering/consistency/RelativeWeightConsistencyMetric.java
@@ -28,10 +28,31 @@ public class RelativeWeightConsistencyMetric implements ConsistencyMetric {
     double[] relWeights = new double[subgraph.order()];
     VertexIterator it = subgraph.vertexIterator();
     while (it.hasNext()) {
-      int v = supergraph.localVertexId(it.globalId());
-      assert v >= 0;
-      double supergraphWeight = supergraph.weights()[v];
+      double supergraphWeight = supergraph.weightForGlobalId(it.globalId());
       relWeights[it.localId()] = (supergraphWeight != 0) ? subgraph.weights()[it.localId()] / supergraphWeight : 0;
+    }
+    return relWeights;
+  }
+
+  /**
+   * Compute a the relative weight consistency score for all vertices of a subgraph with respect to one of its supergraphs,
+   * restricted to a sub-subgraph of the supergraph.
+   * That is, return the fraction of subgraph vertex weights compared to those of the same vertex set embedded in the supergraph.
+   *
+   * @param supergraph  A graph
+   * @param subgraph    A subgraph of supergraph
+   * @param subsubgraph A subgraph of subgraph
+   * @return Array of consistency scores for all vertices of the subgraph, restricted to the sub-subgraph
+   */
+
+  @Override
+  public double[] compute(Graph supergraph, Graph subgraph, Graph subsubgraph) {
+    double[] relWeights = new double[subsubgraph.order()];
+    VertexIterator it = subsubgraph.vertexIterator();
+    while (it.hasNext()) {
+      double subgraphWeight = subgraph.weightForGlobalId(it.globalId());
+      double supergraphWeight = supergraph.weightForGlobalId(it.globalId());
+      relWeights[it.localId()] = (supergraphWeight != 0) ? subgraphWeight / supergraphWeight : 0;
     }
     return relWeights;
   }
--- src/test/java/net/adeptropolis/metis/clustering/consistency/RelativeWeightConsistencyMetricTest.java
+++ src/test/java/net/adeptropolis/metis/clustering/consistency/RelativeWeightConsistencyMetricTest.java
@@ -8,6 +8,7 @@ package net.adeptropolis.metis.clustering.consistency;
 import it.unimi.dsi.fastutil.ints.IntIterators;
 import net.adeptropolis.metis.graphs.Graph;
 import net.adeptropolis.metis.graphs.implementations.CompressedSparseGraphBuilder;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -16,22 +17,39 @@ import static org.hamcrest.Matchers.is;
 
 public class RelativeWeightConsistencyMetricTest {
 
-  @Test
-  public void relativeWeights() {
-    Graph graph = new CompressedSparseGraphBuilder()
+  private Graph graph;
+  private Graph subgraph;
+  private RelativeWeightConsistencyMetric metric;
+
+  @Before
+  public void setup() {
+    metric = new RelativeWeightConsistencyMetric();
+    graph = new CompressedSparseGraphBuilder()
             .add(0, 1, 3)
             .add(1, 2, 4)
             .add(2, 0, 5)
             .add(1, 3, 6)
             .add(3, 2, 7)
             .build();
-    Graph subgraph = graph.inducedSubgraph(IntIterators.wrap(new int[]{0, 1, 2}));
-    RelativeWeightConsistencyMetric metric = new RelativeWeightConsistencyMetric();
+    subgraph = graph.inducedSubgraph(IntIterators.wrap(new int[]{0, 1, 2}));
+  }
+
+  @Test
+  public void standardMetric() {
     double[] relWeights = metric.compute(graph, subgraph);
     assertThat(relWeights.length, is(3));
     assertThat(relWeights[0], closeTo(1, 1E-6));
     assertThat(relWeights[1], closeTo(7.0 / 13, 1E-6));
     assertThat(relWeights[2], closeTo(9.0 / 16, 1E-6));
+  }
+
+  @Test
+  public void restrictedMetric() {
+    Graph subsubgraph = subgraph.inducedSubgraph(IntIterators.wrap(new int[]{1, 2}));
+    double[] relWeights = metric.compute(graph, subgraph, subsubgraph);
+    assertThat(relWeights.length, is(2));
+    assertThat(relWeights[0], closeTo(7.0 / 13, 1E-6));
+    assertThat(relWeights[1], closeTo(9.0 / 16, 1E-6));
   }
 
 }
